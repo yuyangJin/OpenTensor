d1e22f1681c040552c0374f623b304b38dab70e1
yuyangJin <yuyuanjin47@gmail.com>
2023-01-12 21:51:35 +0800

    Add matmul example in C++

---
+++ test/matmul.cpp
@@ -0,0 +1,15 @@
+#include "opentensor.h"
+
+int main() {
+    Tensor A;
+    A.ot_random(3 * 4);
+    Tensor B;
+    B.ot_random(4 * 5);
+    Tensor C;
+    C.ot_zeros(3 * 5);
+
+    Dim i, j, k;
+    // C["ij"] = (A["ik"] * B["kj"]);//.sum(k);
+    C[i][j].einsum((A[i][k] * B[k][j]).sum(k));
+
+}
