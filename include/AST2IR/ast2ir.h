1bffb8baebf40d3d16c13186736b6f4eeda06b2d
yuyangJin <yuyuanjin47@gmail.com>
2023-01-13 13:05:06 +0800

    Update AST2IR: replace RecursiveASTVisitor by ASTMatcher

--- include/AST2IR/ast2ir.h
+++ include/AST2IR/ast2ir.h
@@ -8,6 +8,8 @@
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/Expr.h"
 #include "clang/AST/RecursiveASTVisitor.h"
+#include <clang/ASTMatchers/ASTMatchFinder.h>
+#include <clang/ASTMatchers/ASTMatchers.h>
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Tooling/CommonOptionsParser.h"
@@ -26,52 +28,90 @@
 
 std::unique_ptr<IRGraph> _graph;
 
-class ASTConverterClassVisitor
-    : public clang::RecursiveASTVisitor<ASTConverterClassVisitor> {
-private:
-  clang::ASTContext *astContext; // used for getting additional AST info
-  std::vector<clang::ArraySubscriptExpr *> visitedASE;
-  int recursiveCount = 0;
+// class ASTConverterClassVisitor
+//     : public clang::RecursiveASTVisitor<ASTConverterClassVisitor> {
+// private:
+//   clang::ASTContext *astContext; // used for getting additional AST info
+//   std::vector<clang::ArraySubscriptExpr *> visitedASE;
+//   int recursiveCount = 0;
 
-public:
-  explicit ASTConverterClassVisitor(clang::CompilerInstance *CI)
-      : astContext(&(CI->getASTContext())) {}
+// public:
+//   explicit ASTConverterClassVisitor(clang::CompilerInstance *CI)
+//       : astContext(&(CI->getASTContext())) {}
 
-  virtual bool VisitFunctionDecl(clang::FunctionDecl *func) {
-    std::string funcName = func->getNameInfo().getName().getAsString();
-    llvm::errs() << "Function: " << funcName << "\n";
-    return true;
-  }
+//   virtual bool VisitFunctionDecl(clang::FunctionDecl *func) {
+//     // std::string funcName = func->getNameInfo().getName().getAsString();
+//     // llvm::errs() << "Function: " << funcName << "\n";
+//     return true;
+//   }
 
-  virtual bool VisitArraySubscriptExpr(clang::ArraySubscriptExpr *array) {
-    return true;
-  }
+//   virtual bool VisitArraySubscriptExpr(clang::ArraySubscriptExpr *array) {
+//     return true;
+//   }
 
-  virtual bool VisitStmt(clang::Stmt *st) {
-    if (auto *ret = llvm::dyn_cast<clang::ReturnStmt>(st)) {
+//   virtual bool VisitStmt(clang::Stmt *st) {
+//     if (auto *ret = llvm::dyn_cast<clang::ReturnStmt>(st)) {
+//     }
+//     if (auto *call = llvm::dyn_cast<clang::CallExpr>(st)) {
+//     }
+//     return true;
+//   }
+
+//   // virtual bool VisitDeclStmt(clang::DeclStmt *decl) {
+//   //   clang::Stmt::child_range children = decl->children();
+//   //   for (clang::Stmt* child: children) {
+//   //     if (llvm::isa<clang::VarDecl>(child)) {
+//   //       clang::VarDecl *var_decl = (clang::VarDecl*)child;
+//   //       var_decl->getType().dump();
+//   //     }
+//   //   }
+//   //   return true;
+//   // }
+
+//   // virtual bool VisitVarDecl(clang::VarDecl *vd) ;
+
+//   virtual bool VisitMemberExpr(clang::MemberExpr *me) ;
+// };
+
+
+class ASTConverterCallback : public clang::ast_matchers::MatchFinder::MatchCallback {
+ public:
+  ASTConverterCallback(){}
+  void run(const clang::ast_matchers::MatchFinder::MatchResult &Result) override {
+    const auto* func = Result.Nodes.getNodeAs<clang::FunctionDecl>("fn");
+    if (func) {
+      std::string funcName = func->getNameInfo().getName().getAsString();
+      llvm::errs() << "Function: " << funcName << "\n";
     }
-    if (auto *call = llvm::dyn_cast<clang::CallExpr>(st)) {
+    const clang::VarDecl *vd = Result.Nodes.getNodeAs<clang::VarDecl>("VarDecl_in_main");
+    if (vd) {
+      llvm::errs() << vd->getType().getAsString() << "\n";
+      llvm::errs() << vd->getNameAsString() << "\n";
     }
-    return true;
+    
   }
-
-  virtual bool VisitReturnStmt(clang::ReturnStmt *ret) { return true; }
-
-  virtual bool VisitCallExpr(clang::CallExpr *call) { return true; }
 };
+
 
 class ASTConverterClassConsumer : public clang::ASTConsumer {
 private:
-  ASTConverterClassVisitor *visitor; // doesn't have to be private
+  // ASTConverterClassVisitor *visitor; // doesn't have to be private
+  clang::ast_matchers::MatchFinder match_finder; 
+  ASTConverterCallback handler; 
 
 public:
   // override the constructor in order to pass CI
   explicit ASTConverterClassConsumer(clang::CompilerInstance *CI) {
-    visitor = new ASTConverterClassVisitor(CI);
+    match_finder.addMatcher(clang::ast_matchers::varDecl(clang::ast_matchers::isExpansionInMainFile()).bind("VarDecl_in_main"), &handler);
+    const auto matching_node = clang::ast_matchers::functionDecl(clang::ast_matchers::isExpansionInMainFile()).bind("fn");
+        match_finder.addMatcher(matching_node, &handler);
+    // visitor = new ASTConverterClassVisitor(CI);
+    // match_finder.addMatcher(clang::ast_matchers::varDecl(clang::ast_matchers::hasAncestor(clang::ast_matchers::functionDecl())).bind("VarDecl_hasAncestor_functionDecl"), &handler);
   }
 
   virtual void HandleTranslationUnit(clang::ASTContext &Context) {
-    visitor->TraverseDecl(Context.getTranslationUnitDecl());
+    // visitor->TraverseDecl(Context.getTranslationUnitDecl());
+    match_finder.matchAST(Context);
   }
 };
 
--- lib/AST2IR/ast2ir.cpp
+++ lib/AST2IR/ast2ir.cpp
@@ -1,1 +1,34 @@
 #include "AST2IR/ast2ir.h"
+
+#define FUNC_LIST_SIZE 10
+std::string builtin_func_list[] = {
+  "random",
+  "zeros",
+  "print"
+};
+
+bool isBuiltinFunc(std::string & func) {
+  for (size_t i = 0; i < FUNC_LIST_SIZE; i++) {
+    if (func == builtin_func_list[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// bool ASTConverterClassVisitor::VisitVarDecl(clang::VarDecl *vd) {
+//   // llvm::errs() << vd->getType().getAsString() << "\n";
+//   // llvm::errs() << vd->getName().str() << "\n";
+//   return true;
+// }
+
+// bool ASTConverterClassVisitor::VisitMemberExpr(clang::MemberExpr *me) {
+//   auto func_name = me->getMemberNameInfo().getAsString();
+//   if(isBuiltinFunc(func_name)) {
+//     llvm::errs() << "Builtin:";
+//   }
+//   llvm::errs() << me->getMemberNameInfo().getAsString() << "\n"; 
+  
+//   return true;
+// }
+
