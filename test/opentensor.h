73218ee101ac50274752aa97dee5aec709368b3a
yuyangJin <yuyuanjin47@gmail.com>
2023-01-15 13:04:23 +0800

    Update test case

--- test/opentensor.h
+++ test/opentensor.h
@@ -7,14 +7,15 @@ class Tensor {
   double *tensor;
 
 public:
-  template<typename T, typename... Ts>
-  Tensor (T dim1, Ts... dimn);
+  template <typename T, typename... Ts> Tensor(T dim1, Ts... dimn);
   void random();
   void zeros();
   void print();
   Tensor &operator[](Dim &);
   Tensor &operator*(Tensor &);
+  // template <typename T, typename... Ts> Tensor &sum(T dim1, Ts... dimn);
   Tensor &sum(Dim &);
+  Tensor &sum(Dim &, Dim &);
   Tensor &einsum(Tensor &);
 };
 
